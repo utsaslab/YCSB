51bb194c6c5f73e0db1fd4cea63882e766605614
William Lin <wlsaidhi@gmail.com>
2017-12-09 13:44:26 -0600

    created generalized pareto distribution generator

---
+++ core/src/main/java/com/yahoo/ycsb/generator/GeneralizedParetoGenerator.java
@@ -0,0 +1,42 @@
+package com.yahoo.ycsb.generator;
+
+import com.yahoo.ycsb.Utils;
+
+/**
+ * A generator of a generalized pareto distribution.
+ */
+public class GeneralizedParetoGenerator extends NumberGenerator {
+  private final double mu;
+  private final double sigma;
+  private final double xi;
+
+  public GeneralizedParetoGenerator(final double mu, final double sigma, 
+      final double xi) {
+    this.mu = mu;
+    this.sigma = sigma;
+    this.xi = xi;
+  }
+
+  @Override
+  public Double nextValue() {
+    // TODO: handle xi = 0 case
+    return mu + 
+      ((sigma * (Math.pow(Utils.random().nextDouble(), -xi) - 1)) / xi);
+  }
+
+  @Override
+  public double mean() {
+    return mu + (sigma / (1 - xi));
+  }
+
+  //public static void main(String[] args) {
+    //GeneralizedParetoGenerator g = new GeneralizedParetoGenerator(90, 100);
+    //int j = 0;
+    //for (int i = 0; i < 1000; i++) {
+      //if (e.nextValue() < 100) {
+        //j++;
+      //}
+    //}
+    //System.out.println("Got " + j + " hits.  Expect 900");
+  //}
+}

43581ca1b14e453a2205bfd42b9dfe89ecea0d06
William Lin <wlsaidhi@gmail.com>
2017-12-24 14:35:28 -0600

    added println to test (commented out)

--- core/src/test/java/com/yahoo/ycsb/generator/TestFacebookFieldLengthGenerator.java
+++ core/src/test/java/com/yahoo/ycsb/generator/TestFacebookFieldLengthGenerator.java
@@ -4,8 +4,7 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertFalse;
 
 import java.io.IOException;
-import java.nio.file.*;
-
+import java.util.*;
 
 public class TestFacebookFieldLengthGenerator {
   @Test
@@ -14,11 +13,23 @@ public class TestFacebookFieldLengthGenerator {
     try {
       FacebookFieldLengthGenerator fb = new FacebookFieldLengthGenerator();
 
+      SortedMap<Long, Integer> m = new TreeMap<Long, Integer>();
       for (int i = 0; i < 10000; i++) {
         long rnd = fb.nextValue();
+        if (m.containsKey(rnd))
+          m.put(rnd, m.get(rnd) + 1);
+        else
+          m.put(rnd, 1);
         //System.out.println(rnd);
         assertFalse(rnd < min);
       }
+
+      //for(Map.Entry<Long,Integer> entry : m.entrySet()) {
+        //Long key = entry.getKey();
+        //Integer value = entry.getValue();
+
+        //System.out.println(key + " => " + value);
+      //}
     } catch (IOException e) {
       System.out.println(e.getMessage());
       assertFalse(true);

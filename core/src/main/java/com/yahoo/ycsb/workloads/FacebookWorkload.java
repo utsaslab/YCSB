94b1b6c1391dfeee36888f2b1738111385577f1a
William Lin <wlsaidhi@gmail.com>
2017-12-30 03:26:16 -0600

    FacebookWorkload now uses a keylengthgenerator

--- core/src/main/java/com/yahoo/ycsb/workloads/FacebookWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/FacebookWorkload.java
@@ -2,7 +2,7 @@ package com.yahoo.ycsb.workloads;
 
 import com.yahoo.ycsb.Utils;
 import com.yahoo.ycsb.WorkloadException;
-import com.yahoo.ycsb.generator.FacebookFieldLengthGenerator;
+import com.yahoo.ycsb.generator.*;
 
 import java.util.Properties;
 import java.io.IOException;
@@ -13,6 +13,13 @@ import java.io.IOException;
  * CoreWorkload} and overrides necessary methods like init, buildKeyName etc.
  */
 public class FacebookWorkload extends CoreWorkload {
+
+  protected NumberGenerator keylengthgenerator;
+
+  private static final double EXTREME_VALUE_LOCATION = 30.7984;
+  private static final double EXTREME_VALUE_SCALE = 8.20449;
+  private static final double EXTREME_VALUE_SHAPE = -0.078688;
+
   @Override
   public void init(Properties p) throws WorkloadException {
     super.init(p);
@@ -21,20 +28,33 @@ public class FacebookWorkload extends CoreWorkload {
     } catch (IOException e) {
       throw new WorkloadException("Could not create field length generator.\n");
     }
+
+    keylengthgenerator = new GeneralizedExtremeValueGenerator(
+        EXTREME_VALUE_LOCATION, 
+        EXTREME_VALUE_SCALE, 
+        EXTREME_VALUE_SHAPE);
   }
 
-  // TODO: use the FacebookFieldLengthGenerator
+  // NOTE: This function might break the ordering of the keys.
   @Override
   protected String buildKeyName(long keynum) {
+    int size = keylengthgenerator.nextValue().intValue();
+    StringBuilder sb = new StringBuilder(size);
+
     if (!orderedinserts) {
       keynum = Utils.hash(keynum);
     }
     String value = Long.toString(keynum);
-    int fill = zeropadding - value.length();
-    String prekey = "user";
-    for (int i = 0; i < fill; i++) {
-      prekey += '0';
+    String prekey = "";
+    if (value.length() < size) {
+      int fill = size - value.length();
+      for (int i = 0; i < fill; i++) {
+        prekey += '0';
+      }
     }
-    return prekey + value;
+    sb.append(prekey);
+    sb.append(value);
+    sb.setLength(size);
+    return sb.toString();
   }
 }

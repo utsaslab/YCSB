1f1bc1ab0745a4f450cebf00fcf388675183ee28
William Lin <wlsaidhi@gmail.com>
2018-01-07 00:45:29 -0600

    added some basic scripts for running a workload

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# config vars for run.sh
+
+MAVEN_GROUP_ID="com.yahoo.ycsb"
+DB_INTERFACE_SUFFIX="-binding"
+YCSB_COMMAND_PATH="bin/ycsb"
+YCSB_FLAGS="-s"
+
+LOCALHOST="127.0.0.1"
+
+# redis
+REDIS_PORT="6379"
---
+++ util.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# util functions for scripts
+
+bold()          { ansi 1 "$@"; }
+italic()        { ansi 3 "$@"; }
+underline()     { ansi 4 "$@"; }
+strikethrough() { ansi 9 "$@"; }
+red()           { ansi 31 "$@"; }
+ansi()          { echo -e "\e[${1}m${*:2}\e[0m"; }
+
+error() {
+  echo "ERROR: ${@}" 1>&2
+  exit 1
+}
+
+warn() {
+  echo "WARNING: ${@}" 1>&2
+}

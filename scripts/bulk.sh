939b29a59046d826c2cf73b09b93b8b82ba11b6d
William Lin <wlsaidhi@gmail.com>
2018-09-16 09:09:10 -0700

    updated pom.xml for leveldb

--- scripts/bulk.sh
+++ scripts/bulk.sh
@@ -6,11 +6,11 @@ for t in fieldlength keylength; do
     for db in rocksdb leveldb; do
         for l in a b c d e f; do
             echo $t $db $l
-            #./scripts/run.sh -w workloads/workload${l} -db $db -i \
-                #${db}_w${l} -s
+            ./scripts/run.sh -w workloads/workload${l} -db $db -i \
+                ${db}_w${l} -s
         done
 
-        #./scripts/run.sh -w workloads/facebookworkload -db \
-            #$db -i ${db}_fb -s
+        ./scripts/run.sh -w workloads/facebookworkload -db \
+            $db -i ${db}_fb -s
     done
 done 
